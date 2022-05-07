b78c7ff65d35b0f81e00a4066c9619c50093d81d
heterodain <46586035+heterodain@users.noreply.github.com>
2022-05-08 03:01:24 +0900

    利用ライブラリ変更

--- bh1750fvi-lightsensor.ino
+++ bh1750fvi-lightsensor.ino
@@ -1,31 +1,36 @@
 /*
-  Connecting the sensor to a Arduino UNO / Nano:
-  VCC  <-> 3V3
+  BH1750FVI と Arduino UNO/Nano の接続:
+  VCC  <-> 3V3 or 5V
   GND  <-> GND
   SDA  <-> A4/SDA 
   SCL  <-> A5/SCL
 */
 
-#include <BH1750FVI.h>
+#include <BH1750.h>
 
-BH1750FVI LightSensor(BH1750FVI::k_DevModeOneTimeHighRes);
+BH1750 lightSensor;
 
-void setup() 
-{
+void setup() {
   Serial.begin(9600);
   Serial.setTimeout(100);
-  LightSensor.begin();  
+
+  Wire.begin();
+  lightSensor.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
 }
 
-void loop()
-{
-  while (Serial.available() == 0) {
-    delay(100);
+void loop() {
+  while (!Serial.available()) {
+    delay(50);
   }
   
   String input = Serial.readString();
-  if (input == "GET") {
-    uint16_t lux = LightSensor.GetLightIntensity();
+  if (input.startsWith("GET")) {
+    lightSensor.configure(BH1750::ONE_TIME_HIGH_RES_MODE);
+    while (!lightSensor.measurementReady(true)) {
+      delay(10);
+    }
+
+    float lux = lightSensor.readLightLevel();
     Serial.println(lux);
   }
 }
